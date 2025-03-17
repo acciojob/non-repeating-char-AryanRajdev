5d9d6b5a426d41b8f43681136b15649be89b46c9
AryanRajdev <91838247+AryanRajdev@users.noreply.github.com>
2025-03-17 23:52:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = new Array(26).fill(0);
+
+	for(let i =0 ; i<str.length ; i++){
+		let ch = str[i];
+		arr[ch.charCodeAt(0)-'a'.charCodeAt(0)]++;
+	}
+
+	let ans = "null";
+
+	for(let i =0 ;i<26 ; i++){
+		if(arr[i] === 1){
+			ans = String.fromCharCode(i+'a'.charCodeAt(0));
+			break;
+		}
+	}
+
+	return ans;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
